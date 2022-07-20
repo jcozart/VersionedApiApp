051d9692341ed28c16d09dcfd04dd781bc4d5978
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-07-20 08:58:30 -0500

    Add project files.

---
+++ VersionedApi/Controllers/v1/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace VersionedApi.Controllers.v1;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0", Deprecated = true)]
+public class UsersController : ControllerBase
+{
+    // GET api/v1/Users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "Version 1 value 1", " Version 1 value 2" };
+    }
+}
---
+++ VersionedApi/Program.cs
@@ -0,0 +1,79 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(opts =>
+{
+    var title = "Our Versioned API";
+    var description = "This is a WEB API that demonstrates versioning.";
+    var terms = new Uri("https://localhost:7089/terms");
+    var license = new OpenApiLicense()
+    {
+        Name = "This is my full license information or a link to it."
+    };
+
+    var contact = new OpenApiContact()
+    {
+        Name = "Jordan Cozart",
+        Email = "jordancozart10@gmail.com",
+        Url = new Uri("https://www.jordancozart.com")
+    };
+
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = $"{title} v1 (deprecated)",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+
+    opts.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = $"{title} v2",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+});
+
+builder.Services.AddApiVersioning(opts =>
+{
+    opts.AssumeDefaultVersionWhenUnspecified = true;
+    opts.DefaultApiVersion = new(2, 0);
+    opts.ReportApiVersions = true;
+});
+
+builder.Services.AddVersionedApiExplorer(opts =>
+{
+    opts.GroupNameFormat = "'v'VVV";
+    opts.SubstituteApiVersionInUrl = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
